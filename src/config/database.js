8d171a923ee7b473ac4176cfeea92b8df63c0943
Weslei França <weslwifranca@gmail.com>
2024-10-03 16:17:02 -0300

    update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,14 @@
+const dotenv = require('dotenv')
+const pg = require('pg')
+dotenv.config()
+
 module.exports = {
   dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'postgres',
-  database: 'postgres',
+  dialectModule: pg,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   define: {
     timespamps: true,
     underscored: true,
